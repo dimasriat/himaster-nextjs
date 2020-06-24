75e025affae8e4c423f7d58c2325a7a308060efc
Dimas <dimasriat@gmail.com>
2020-06-25 00:26:52 +0700

    contact page added, profile page added

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,52 @@
 import Layout from "../components/Layout";
 import { Container } from "../components/Mixin";
 
+const gmaps = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1977.5736082916894!2d110.85753515783222!3d-7.5589226986371685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a1703a9577c13%3A0x71e559b1b5110e41!2sFakultas%20MIPA%20Universitas%20Sebelas%20Maret!5e0!3m2!1sid!2sid!4v1593011272312!5m2!1sid!2sid" style="width: 100%; height: 480px; border: 1px solid black;" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`;
+
+function CardSosmed(props) {
+	return (
+		<div className="col">
+			<div className="card shadow">
+				<div className="card-body">{props.children}</div>
+			</div>
+		</div>
+	);
+}
+
 export default function ContactPage(props) {
-	const teams = props.teams;
 	return (
 		<Layout>
 			<Container>
 				<div className="row pb-3 m__h">
-					<div className="col-12">
-						<p>Contact Page</p>
+					<div className="col-lg col-12">
+						{/* <p>Contact Page</p> */}
+						<div dangerouslySetInnerHTML={{ __html: gmaps }} />
+					</div>
+					<div className="col-lg col-12">
+						<h2 className="h1">Contact Us</h2>
+						<h3 className="mt-3">Alamat</h3>
+						<div className="row">
+							<CardSosmed>
+								Sekretariat: Lt.1 Gd.A FMIPA UNS Jl. Ir. Sutami
+								36 A Surakarta 57126
+							</CardSosmed>
+						</div>
+						<div className="row mt-3">
+							<CardSosmed>himaster.mipa.uns@gmail.com</CardSosmed>
+						</div>
+						<h3 className="mt-3">Follow Us</h3>
+						<div className="row">
+							<CardSosmed>
+								<a href="https://instagram.com/himaster.uns">
+									Instagram
+								</a>
+							</CardSosmed>
+							<CardSosmed>
+								<a href="https://twitter.com/himaster_uns">
+									Twitter
+								</a>
+							</CardSosmed>
+						</div>
 					</div>
 				</div>
 				<style jsx>{`
--- pages/profil.js
+++ pages/profil.js
@@ -1,6 +1,26 @@
 import Layout from "../components/Layout";
 import { Container, VisiMisi } from "../components/Mixin";
 
+function LogoLayout(props) {
+	const [orderLogo, orderText] = props.switch ? [1, 2] : [2, 1];
+	return (
+		<div className="row pb-3 align-items-center">
+			<div className={`col-lg col-12 py-3 order-lg-${orderLogo}`}>
+				<img src={props.src} className="img-fluid" />
+			</div>
+			<div className={`col-lg col-12 py-3 order-lg-${orderText}`}>
+				<ol className="m-0">
+					{props.children.map((item) => (
+						<li key={item} className="text-justify mb-2">
+							{item}
+						</li>
+					))}
+				</ol>
+			</div>
+		</div>
+	);
+}
+
 function FilosofiLogo() {
 	const filosofi = [
 		"Lambang alpha itu berarti terdepan sama dengan filosofi nama HIMASTER ALPHA",
@@ -19,20 +39,27 @@ function FilosofiLogo() {
 					</p>
 				</div>
 			</div>
-			<div className="row pb-3 align-items-center">
-				<div className="col-lg col-12 py-3">
-					<img src="/img/alpha-logo-big.png" className="img-fluid"/>
-				</div>
-				<div className="col-lg col-12 py-3">
-					<ol className="m-0">
-						{filosofi.map((item) => (
-							<li key={item} className="text-justify mb-2">
-								{item}
-							</li>
-						))}
-					</ol>
-				</div>
-			</div>
+			<LogoLayout src="/img/himaster_ori.png">
+				{[
+					"Tulisan Universitas Sebelas Maret menunjukkan bahwa HIMASTER berkedudukan di bawah naungan Universitas Sebelas Maret.",
+					"Bentuk perisai menggambarkan bahwa HIMASTER memiliki filosofi layaknya sebuah antivirus yang bisa memberikan solusi atas masalah-masalah yang terdapat di masyarakat dengan mengaplikasikan ilmu yang dimilikinya.",
+					"Cahaya di dalam perisai menggambarkan bahwa HIMASTER berusaha memberikan manfaat dalam bidang teknologi informasi dan komunikasi kepada masyarakat.",
+					"Huruf “I” dan “K” pada bagian tengah menunjukkan Program Studi S-1 Informatika yang sebelumnya bernama Jurusan S-1 Ilmu Komputer Fakultas Matematika dan Ilmu Pengetahuan Alam Universitas Sebelas Maret.",
+					"Pemancar yang menyusun huruf “I” bermakna bahwa HIMASTER selalu mengembangkan jaringan dan kerja sama yang baik dengan pihak universitas dan/atau luar universitas.",
+					"Laptop yang menyusun huruf “K” mewakili identitas dari mahasiswa Program Studi S-1 Informatika.",
+					"Bentuk pita yang di dalamnya terdapat tulisan HIMASTER menggambarkan hubungan yang erat dan solid antarpengurus HIMASTER.",
+				]}
+			</LogoLayout>
+			<LogoLayout src="/img/alpha-logo-big.png" switch>
+				{[
+					"Lambang alpha itu berarti terdepan sama dengan filosofi nama HIMASTER ALPHA",
+					"Lambang alpha yang diletakkan ditengah berartikan bahwa HIMASTER ALPHA ini ditempatkan untuk berperan aktif dan seimbang membangun Informatika ini secara internal maupun eksternal",
+					"Pemilihan warna solid sekaligus eksentrik sebagai harapan HIMASTER ALPHA bisa menjadi organisasi yang berbasis kekeluargaan dan menjadi organisasi yang kreatif dan berani beda",
+					"Kolaborasi merupakan perwujudan dari misi HIMASTER ALPHA untuk membangun sinergi, relasi, kolaborasi, serta kontribusi baik kepada internal maupun eksternal HIMASTER",
+					"Aksi merupakan perwujudan dari sikap anggota HIMASTER untuk bersinergi serta membangun HIMASTER menjadi organisasi yang lebih baik dan dapat bermanfaat bagi anggota serta masyarakatnya",
+					"Kreasi merupakan perwujudan dari hasil karya anggota HIMASTER untuk membangun HIMASTER menjadi organisasi yang lebih baik dan dapat dirasakan manfaatnya baik untuk anggota maupun masyarakatnya",
+				]}
+			</LogoLayout>
 		</Container>
 	);
 }
